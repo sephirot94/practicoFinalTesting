83fd9967739736d413d2c788d289f0f6fd77e406
AlumnoUB <AlumnoUB@Admin-PC>
2017-06-16 21:12:34 -0300

    trabajando main

--- src/Truco/Juego.java
+++ src/Truco/Juego.java
@@ -14,14 +14,27 @@ public class Juego {
 	}
 
 	public static void main(String[] args) {
+		Mazo mazo = new Mazo();
+		Menu menu = new Menu();
 		Juego.setFin(false);
+		Integer cantidad_jugadores = menu.menuInicial();
+		String nombre_jugador = menu.menuNombre();
+		Equipo equipo_jugador = new Equipo(cantidad_jugadores, nombre_jugador);
+		Equipo equipo_oponente = new Equipo(cantidad_jugadores);
 		do{
+			mazo.mezclarMazo();
 			Ronda ronda = new Ronda();
-			Menu menu = new Menu();
-			Integer cantidad_jugadores = menu.menuInicial();
-			String nombre_jugador = menu.menuNombre();
-			Equipo equipo_jugador = new Equipo(cantidad_jugadores, nombre_jugador);
-			Equipo equipo_oponente = new Equipo(cantidad_jugadores);
+			Integer posicion = 0;
+			for(Jugador jugador : equipo_jugador.getIntegrantes()){
+				Mano mano = new Mano(mazo.getMazo().get(posicion), mazo.getMazo().get(posicion + 1), mazo.getMazo().get(posicion + 2));
+				jugador.setMano(mano);
+				posicion+= 3;
+			}
+			for(Jugador jugador : equipo_oponente.getIntegrantes()){
+				Mano mano = new Mano(mazo.getMazo().get(posicion), mazo.getMazo().get(posicion + 1), mazo.getMazo().get(posicion + 2));
+				jugador.setMano(mano);
+				posicion+= 3;
+			}
 			menu.menuFinal();
 		}while(!Juego.getFin());
 	} 
